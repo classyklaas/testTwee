ff1324c392c2a48e034543b2ffd698bfa0b47e0e
classyklaas <klaasjankostelijk@gmail.com>
2020-06-07 15:35:03 +0200

    test twee

---
+++ testTwee.java
@@ -0,0 +1,7 @@
+public class testTwee {
+        public static void main(String[] args){
+            System.out.println("Test 2");
+//            System.out.println("Hey again!");
+        }
+    }
+
